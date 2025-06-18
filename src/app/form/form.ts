b84fdfce9f1260596b5f90eedb186831064851b7
mdohr07 <124890015+mdohr07@users.noreply.github.com>
2025-06-18 15:59:20 +0200

    add a toggle for login and register

--- src/app/form/form.ts
+++ src/app/form/form.ts
@@ -10,13 +10,29 @@ import { FormsModule } from '@angular/forms';
 })
 
 export class Form {
+  mode: 'login' |'register' = 'login';
   username: string = '';
   password: string = '';
+  confirmPassword: string = '';
+
   remember: boolean = true;
 
+  toggleMode() {
+    this.mode = this.mode === 'login' ? 'register' : 'login';
+  }
+
   onSubmit() {
-    console.log('Username:', this.username);
-    console.log('Password:', this.password);
-    console.log('Remember Me:', this.remember);
+    if (this.mode === 'register') {
+        if (this.password !== this.confirmPassword) {
+          console.log('The passwords do not match')
+          return;
+    } 
+    console.log('Register:', this.username, this.password);
+    } else {
+      console.log('Login:', this.username, this.password);
+    }
+    // console.log('Username:', this.username);
+    // console.log('Password:', this.password);
+    // console.log('Remember Me:', this.remember);
   }
 }
